f9946739f81f01692f09dc74e0502ab3f9ad4b11
Mando Rodriguez <Armando.Rodriguez@utsa.edu>
2016-02-01 17:50:19 -0600

    1. added some code for loading into dicts for lookups across tables.

--- mouse_to_human.py
+++ mouse_to_human.py
@@ -5,7 +5,38 @@ import os
 import sys
 import pdb
 
+#-------------------------------------------------------------------------------
 
+class HiSeqData:
+
+    def __init__(self, hiseq_file,column_prefix):
+
+        self.data = {}
+
+        with open(hiseq_file,'r') as hsf:
+            
+            lines = hsf.readlines()
+
+            self.header = [column_id.lstrip(column_prefix) for column_id in lines[0].split()[1:]]
+            
+            for line in lines[1:]:
+            
+                parts = line.split()
+
+                #self.data[parts[0]] = parts[1:]
+
+                row = {}
+                           
+                # putting this into a row that's keyed to the header ids
+                for i,v in enumerate(parts[1:]):
+
+                    row[self.header[i]] = v
+
+                    
+                self.data[parts[0]] = row
+
+
+                pdb.set_trace()
 
 #-------------------------------------------------------------------------------
 # Main function call
@@ -16,11 +47,17 @@ def __main__():
     #                    help="The snp table to input")
     parser.add_argument("-t", "--table", type=str,required=True,
                         help="The table with the conversion")
+    parser.add_argument("-u", "--human", type=str,required=True,
+                        help="Human hiseq data")
+    parser.add_argument("-m", "--mouse", type=str,required=True,
+                        help="Mouse hiseq data")
 
     args = parser.parse_args()
 
 
     table_file = args.table
+    mouse_file = args.mouse
+    human_file = args.human
     
     human = {}
     mouse = {}
@@ -39,7 +76,8 @@ def __main__():
             mouse[parts[3]] = parts[4:]
 
 
-    pdb.set_trace()
+    hiseq_mouse = HiSeqData(mouse_file,"Id4GFP-Seq1_")
+    hiseq_human = HiSeqData(human_file,"HsSgSeq1-")
     
 #-------------------------------------------------------------------------------
 if __name__=="__main__": __main__()
