650d86af975a1787ab44de3145130c72148b48b3
Mando Rodriguez <Armando.Rodriguez@utsa.edu>
2016-02-03 16:36:33 -0600

    1. Script now produces a mouse and human only table.
    2. added a galaxy plugin.

--- mouse_to_human.py
+++ mouse_to_human.py
@@ -33,8 +33,7 @@ class HiSeqData:
 def __main__():
 
     parser = argparse.ArgumentParser()
-    #parser.add_argument("snp_table", type=str,
-    #                    help="The snp table to input")
+
     parser.add_argument("-t", "--table", type=str,required=True,
                         help="The table with the conversion")
     parser.add_argument("-u", "--human", type=str,required=True,
@@ -67,7 +66,7 @@ def __main__():
             parts = line.split()
 
             human_to_mouse[parts[0]] = parts[3] # only going to go human -> mouse
-            #mouse[parts[3]] = parts[0]
+            mouse_to_human[parts[3]] = parts[0]
 
 
     hiseq_mouse = HiSeqData(mouse_file)
@@ -75,33 +74,45 @@ def __main__():
 
     
     new_table = []
+    mouse_exclusive = []
+    human_exclusive = []
 
     for human_id in hiseq_human.data.keys():
 
         try:
             mouse_id = human_to_mouse[human_id]
         except KeyError:
+            # here i collect those human ids without a mouse counterpart
             print "No mouse id for %s" % human_id
             continue
 
-
+        mrow = []
+        hrow = []
+        
         try:
             mrow = hiseq_mouse.data[mouse_id]
         except KeyError:
+            human_exclusive.append([human_id]+hiseq_human.data[human_id])
             print "No mouse data for %s and human id %s" % (mouse_id,human_id)
+
+
+        hrow = hiseq_human.data[human_id] # should never happen be missing
+
+
+        if len(hrow) == 0 or len(mrow) == 0:
             continue
-
-        try:
-            hrow = hiseq_human.data[human_id]
-        except KeyError:
-            print "No human data for %s and mouse id %s" % (human_id,mouse_id)
-            continue
-
-
+        
         new_row = [human_id, mouse_id] + hrow + mrow
         
 
         new_table.append(new_row)
+
+    #---------------------------------------------------------------------------
+    for mouse_id in hiseq_mouse.data.keys():
+        try:
+            mouse_to_human[mouse_id]
+        except KeyError:
+            mouse_exclusive.append([mouse_id]+hiseq_mouse.data[mouse_id])
 
 
     #--- done getting stuff ----------------------------------------------------
@@ -115,6 +126,24 @@ def __main__():
         for row in new_table:
 
             of.write("\t".join(row)+"\n")
+
+    with open("mouse_exclusive.txt",'w') as of:
+
+        of.write("\t".join(["tracking_Id"]+hiseq_mouse.header)+"\n")
+
+        for row in mouse_exclusive:
+
+            of.write("\t".join(row)+"\n")
+
+
+    with open("human_exclusive.txt",'w') as of:
+
+        of.write("\t".join(["tracking_Id"]+hiseq_human.header)+"\n")
+
+        for row in human_exclusive:
+
+            of.write("\t".join(row)+"\n")
+
 
     print "Done!"
     
